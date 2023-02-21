d711741b050c81dca93a1a4944dc665ed231c1df
Seb <107775878+DevgenX@users.noreply.github.com>
2023-02-21 17:36:35 -0500

    Add files via upload

---
+++ phase-4-controller-exception-handling/app/controllers/birds_controller.rb
@@ -0,0 +1,58 @@
+class BirdsController < ApplicationController
+  # Handles the errors
+  rescue_from ActiveRecord::RecordNotFound, with: :render_response_not_found
+
+  # GET /birds
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # GET /birds/:id
+  def show
+    bird = find_bird
+    render json: bird
+  end
+
+  # PATCH /birds/:id
+  def update
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
+  end
+
+  # PATCH /birds/:id/like
+  def increment_likes
+    bird = find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+  end
+
+  # DELETE /birds/:id
+  def destroy
+    bird = find_bird
+    bird.destroy
+    head :no_content
+   end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species, :likes)
+  end
+
+  def find_bird 
+    Bird.find_by(id: params[:id])
+  end
+
+  def render_response_not_found
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+end
